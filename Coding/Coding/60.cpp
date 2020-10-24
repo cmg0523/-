7816bdd64d521ed1c4a7a991e446a46e5c6f64a7
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2020-10-24 14:55:48 +0900

    init

--- Coding/Coding/60.cpp
+++ Coding/Coding/60.cpp
@@ -4,18 +4,24 @@ using namespace std;
 
 int main() {
 	int arr[100] = { 1 };
-	int N;
+	int N, len = 1;
 	cin >> N;
 
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < 100; j++)
+		for (int j = 0; j < len; j++)
 			arr[j] *= 2;
 
-		for (int j = 0; j < 100; j++)
-			if (arr[j] >= 10)
-				arr[j + 1] += 1;
-		
+		if (arr[len - 1] >= 10) len++;
+
+		for (int j = 0; j < len; j++)
+			if (arr[j] >= 10) {
+				arr[j + 1] += arr[j] / 10;
+				arr[j] %= 10;
+			}
 	}
+
+	for (int i = len-1; i >= 0; i--)
+		cout << arr[i];
 
 	return 0;
 }

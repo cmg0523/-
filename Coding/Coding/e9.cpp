e4724938d122d25dd4c8c57c64798d5fb204b9df
nayoung4300 <65953084+nayoung4300@users.noreply.github.com>
2020-11-21 14:32:05 +0900

    init

--- Coding/Coding/e9.cpp
+++ Coding/Coding/e9.cpp
@@ -4,21 +4,20 @@ using namespace std;
 
 int main() {
 	int N[9]; 
-
-		int max = 0, min = 100;
+	int max = 0;
+	int j;
+	
 	for (int i = 0; i < 9; i++) {
 		cin >> N[i];
 
 		if (max < N[i]) {
-			max == N[i];
-		}
-
-		if (min > N[i]) {
-			min == N[i];
+			max = N[i];
+			
 		}
 		
+		
 	}
-	cout << max << " "  << min;
-
+	cout << max;
+	// 몇번째 줄인지 구하는거 아직 못함 물어봐야함
 	return 0;
 }

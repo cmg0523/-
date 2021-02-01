a97aad1a18cf75d59fa6fa9ebe90cd32cb2783a7
최민규 <0523cmg17@naver.com>
2021-02-01 18:44:34 +0900

    e_2

--- 최민규/수열.cpp
+++ 최민규/수열.cpp
@@ -3,31 +3,54 @@
 using namespace std;
 
 int main() {
-	freopen{ "input.txt","r",stdin };
-	int N,n[100],i,count=1,max;
+	freopen("input.txt", "r", stdin);
+	int N, n[100], i, count = 1, max = 0, count2 = 1;
 	cin >> N;
 	for (i = 0; i < N; i++) {
 		cin >> n[i];
 	}
-	for (i = 0; i < N; i++) {
-		if (n[i] <= n[i++]) {
+	for (i = 0; i < N - 1; i++) {
+		if (n[i] > n[i + 1]) {
 			count++;
-			if (n[i] > n[i++]) {
-				max = count;
-				count = 1;
-			}
-			cout << count;
-			if (count < 3) cout << '2';
+			count2 = 1;
 		}
-		if (n[i] >= n[i++]) {
+		else if (n[i] < n[i + 1]) {
+			count2++;
+			count = 1;
+		}
+		else {
 			count++;
-			if (n[i] < n[i++]) {
+			count2++;
+		}
+		if (max < count) max = count;
+		if (max < count2) max = count2;
+	}
+	/*
+	for (i = 0; i < N; i++) {
+		if (n[i] <= n[i + 1]) {
+			count++;
+		}
+		if (n[i] > n[i + 1]) {
+			if (max < count) {
 				max = count;
-				count = 1;
 			}
-			cout << count;
-			if (count < 3) cout << '2';
+			count = 1;
 		}
 	}
+	count = 1;
+	for (i = 0; i < N; i++) {
+		if (n[i] >= n[i + 1]) {
+			count++;
+		}
+		if (n[i] < n[i + 1]) {
+			if (max < count) {
+				max = count;
+			}
+			count = 1;
+		}
+	}*/
+	if (max < 3) cout << '2';
+	else cout << max;
+
 	return 0;
 }

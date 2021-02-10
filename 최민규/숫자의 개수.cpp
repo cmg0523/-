b841eba51ef5fd555bb5599360d8cfa70ca70e2b
최민규 <0523cmg17@naver.com>
2021-02-10 13:33:41 +0900

    e_2

--- 최민규/숫자의 개수.cpp
+++ 최민규/숫자의 개수.cpp
@@ -3,11 +3,22 @@
 using namespace std;
 
 int main() {
-	int A, B, C,count,gop,i;
+	freopen("input.txt", "r", stdin);
+	int A, B, C, count[10] = { 0 }, gop, i,n,n2;
 	cin >> A >> B >> C;
 
 	gop = A * B * C;
+	while (1) {
+		n = gop % 10; 
+		count[n]++;
 
-	for(i=0; i<)
+		gop = gop / 10;
+		if (gop == 0) break;
+
+	}
+	
+	for (i = 0; i < 10; i++) {
+		cout << count[i];
+	}
 	return 0;
 }

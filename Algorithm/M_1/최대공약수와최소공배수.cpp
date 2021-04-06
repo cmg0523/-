ce022fb3c13ce11054e4ac7b24d393baeed970dc
Luigi38 <ericseyoun@naver.com>
2021-04-06 19:16:03 +0900

    m_1

--- Algorithm/M_1/최대공약수와최소공배수.cpp
+++ Algorithm/M_1/최대공약수와최소공배수.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+int gcd_get(int x, int y)
+{
+	if (x % y == 0) return y;    // 나머지가 0이면 y가 최대공약수이다.
+	return (y, x % y);          // x와 y의 최대공약수는 y와 x % y 의 최대공약수와 같다.
+}
+
 int main() {
-	int a, s;
+	int a, s, x, gcd;
 	cin >> a >> s;
+
+	x = gcd_get(a, s);
+
+	gcd = x * (a / x) * (s / x);
+
+	cout << x << gcd;
 	return 0;
 }

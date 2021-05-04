98f45bf626d866abcdb36493dd6ede741a5b4363
mingyuChoi <72682314+cmg0523@users.noreply.github.com>
2021-05-04 18:04:51 +0900

    KOI

---
+++ Algorithm/KOI/KOI/피자 오븐.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N,T,i,t[5];
+	cin >> T;
+	for (i = 1; i <= T; i++) {
+		cin >> N;
+	}
+	for (i = 0; i < 5; i++) {
+
+		cout << t[i];
+	}
+	return 0;
+
+}
